58e2fd80c8e31a1b75f6192da40adec6c5baad8e
jmhockham <jmhockham@gmail.com>
2019-02-05 11:17:48 +0000

    Added a comment on the db init script

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -13,6 +13,7 @@ sudo -u postgres bash -c "psql -c \"CREATE ROLE trans_admin LOGIN PASSWORD 'pass
 
 sudo -u postgres bash -c "psql -c \"CREATE DATABASE trans_db WITH OWNER = con ENCODING = 'UTF8' TABLESPACE = pg_default LC_COLLATE = 'en_GB.UTF-8' LC_CTYPE = 'en_GB.UTF-8' CONNECTION LIMIT = -1 TEMPLATE template0;\""
 sudo -u postgres bash -c "psql -c \"GRANT ALL ON DATABASE trans_db TO trans_admin;\""
+#The user "fc" is my default Postgres admin
 sudo -u postgres bash -c "psql -c \"GRANT ALL ON DATABASE trans_db TO fc;\""
 sudo -u postgres bash -c "psql -c \"REVOKE ALL ON DATABASE trans_db FROM public;\""
 
